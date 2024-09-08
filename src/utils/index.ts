912ac986eced4d91b9fa23adbd5334575f2427cc
YanPuty <yanputy123@gmail.com>
2024-09-09 06:59:49 +0700

    update env example

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,14 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 export class Utils {
 
   static generateVerificationCode(): string {
-    // Generate a random 6-digit number and pad with leading zeros if necessary
-    return Math.floor(100000 + Math.random() * 900000).toString();
+    if (process.env.ENABLE_EMAIL == "true") {
+      // Generate a random 6-digit number and pad with leading zeros if necessary
+      return Math.floor(100000 + Math.random() * 900000).toString();
+    }
+
+    return '1111';
   }
 }
