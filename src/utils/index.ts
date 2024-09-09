39cfef398a243ec5f0cd7a2a32baff192a800a16
Puty YAN <yan.puty@ababank.com>
2024-09-09 15:11:23 +0700

    Enahancement on Verification Code Generator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export class Utils {
 
   static generateVerificationCode(): string {
-    if (process.env.ENABLE_EMAIL == "true") {
+    if (process.env.VERIFICATION_CODE_ENABLED == "true") {
       // Generate a random 6-digit number and pad with leading zeros if necessary
       return Math.floor(100000 + Math.random() * 900000).toString();
     }
