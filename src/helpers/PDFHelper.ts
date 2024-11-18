027d30ca09ca3b4ee6ed460db7f1c9982815b4a5
Puty YAN <yan.puty@ababank.com>
2024-11-18 13:47:53 +0700

    Fixed PDF error

--- src/helpers/PDFHelper.ts
+++ src/helpers/PDFHelper.ts
@@ -26,10 +26,7 @@ export class PdfHelper {
       const template = await this.loadTemplate();
       const htmlContent = template(data);
 
-      const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      });
+      const browser = await puppeteer.launch();
       const page = await browser.newPage();
       await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
 
